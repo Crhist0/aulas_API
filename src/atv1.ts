26b8f2a0d6d1e856e947897f0ec672826e1d9781
Diener Dornelas <dienezim@hotmail.com>
2022-09-15 19:53:26 -0300

    refactor functions

---
+++ src/atv1.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+async function getUserFromGithub(user: string) {
+  try{
+    const { data } = await axios.get(`https://api.github.com/users/${user}`);
+    console.log(data);
+    return data;
+  }catch(error){
+    console.error("Usuário não existe", error)
+  }
+    // .then((response) => {
+    //   console.log(response.data);
+    // })
+    // .catch((err) => {
+    //   console.log('Usuário não existe');
+    // });
+}
+// getUserFromGithub('djunior97');
+// getUserFromGithub('djunioriqdivqv97');
+
+
+async function getRepositories(repo: string) {
+  try{
+    const {data} = await axios.get(`https://api.github.com/repos/${repo}`)
+    console.log(data);
+    return data;
+  }catch(error) {
+    console.error('Repositório não existe', error);
+  }
+}
+
+
+// Github.getRepositories('marcelo-growdev/scrapbook-es6');
+// Github.getRepositories('marcelo-growdev/qdbqqbqwn');
+
+
+export {getRepositories, getUserFromGithub}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from "express";
+import {getRepositories,getUserFromGithub} from './atv1'
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
 app.get("/", (request: Request, response: Response) => {
   return response.send("OK");
+});
+
+app.get("/user/:name",async (req, res)=>{
+  const { name } = req.params;
+  const userGH = await getUserFromGithub(name);
+
+  res.json(userGH);
 });
 
 app.listen(8080, () => console.log("Servidor iniciado"));
