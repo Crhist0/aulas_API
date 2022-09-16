6c7862609a62dd9a1c896b21d8d4b8333bd58a85
RichardWillianx <richardwilliansp@gmail.com>
2022-09-15 21:17:38 -0300

    atv04

--- src/atv1.ts
+++ src/atv1.ts
@@ -1,37 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
 async function getUserFromGithub(user: string) {
-  try{
+  try {
     const { data } = await axios.get(`https://api.github.com/users/${user}`);
     console.log(data);
     return data;
-  }catch(error){
-    console.error("Usuário não existe", error)
+  } catch (error) {
+    console.error("Usuário não existe", error);
   }
-    // .then((response) => {
-    //   console.log(response.data);
-    // })
-    // .catch((err) => {
-    //   console.log('Usuário não existe');
-    // });
+  // .then((response) => {
+  //   console.log(response.data);
+  // })
+  // .catch((err) => {
+  //   console.log('Usuário não existe');
+  // });
 }
 // getUserFromGithub('djunior97');
 // getUserFromGithub('djunioriqdivqv97');
 
-
 async function getRepositories(repo: string) {
-  try{
-    const {data} = await axios.get(`https://api.github.com/repos/${repo}`)
+  try {
+    const { data } = await axios.get(`https://api.github.com/repos/${repo}`);
     console.log(data);
     return data;
-  }catch(error) {
-    console.error('Repositório não existe', error);
+  } catch (error) {
+    console.error("Repositório não existe", error);
   }
 }
-
 
 // Github.getRepositories('marcelo-growdev/scrapbook-es6');
 // Github.getRepositories('marcelo-growdev/qdbqqbqwn');
 
-
-export {getRepositories, getUserFromGithub}
+export { getRepositories, getUserFromGithub };
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,55 @@
 import express, { Request, Response } from "express";
-import {getRepositories,getUserFromGithub} from './atv1'
+import { getRepositories, getUserFromGithub } from "./atv1";
 import calcular from "./atv2";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 app.get("/", (request: Request, response: Response) => {
   return response.send("OK");
 });
 
-app.get("/user/:name",async (req, res)=>{
+app.get("/user/:name", async (req, res) => {
   const { name } = req.params;
   const userGH = await getUserFromGithub(name);
 
   res.json(userGH);
 });
 
-app.get("/calculadora", (req, res)=>{
+app.get("/calculadora", (req, res) => {
   const { operacao, valorA, valorB } = req.query;
   try {
-    if(!operacao || !valorA || !valorB){
-      return res.status(404).send("<h1>Falta parametro</h1>")
-    } 
-    const response = calcular(operacao as string, Number(valorA), Number(valorB))
-    return res.status(200).send(`<h1>A operaçao ${operacao} com os valores ${valorA} e ${valorB} resultou em ${response}</h1>`)
-  } catch(erro: any){
+    if (!operacao || !valorA || !valorB) {
+      return res.status(404).send("<h1>Falta parametro</h1>");
+    }
+    const response = calcular(
+      operacao as string,
+      Number(valorA),
+      Number(valorB)
+    );
+    return res
+      .status(200)
+      .send(
+        `<h1>A operaçao ${operacao} com os valores ${valorA} e ${valorB} resultou em ${response}</h1>`
+      );
+  } catch (erro: any) {
     console.error({ erro });
-    return res.status(500).send(`<h1>${erro.message}</h1>`)
+    return res.status(500).send(`<h1>${erro.message}</h1>`);
   }
-})
+});
+
+let contador = 0;
+
+app.get("/contador", (req, res) => {
+  contador++;
+  if (contador >= 10) {
+    contador = 0;
+    return res.status(200).send("<h1>Parabéns, você chegou ao 10!</h1>");
+  }
+  return res
+    .status(200)
+    .send(`<h1>O valor atual do contador: ${contador}</h1>`);
+});
 
 app.listen(8080, () => console.log("Servidor iniciado"));
-
